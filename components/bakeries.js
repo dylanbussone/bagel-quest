249b74c45355ff7751b8cd11dddd3f1641b27f4c
Dylan <dylanb@zillow.com>
2020-05-31 18:24:54 -0700

    Add real content

--- components/bakeries.js
+++ components/bakeries.js
@@ -1,13 +1,81 @@
-const bakeries = [];
-
-for (var i = 0; i < 14; i++) {
-    bakeries.push({
-        name: 'Test Bakery',
-        image: '/fake-bakery.jpg',
-        description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent viverra erat diam, et pellentesque nunc iaculis et. Etiam accumsan ac felis finibus egestas. Nullam id felis eget urna vehicula tincidunt. Donec volutpat ligula et sem lobortis, a euismod ligula fringilla.',
-    });
-}
+const bakeries = [
+    {
+        name: 'Bagel Oasis - Ravenna',
+        photoCreditName: '@herbakinglab',
+        photoCreditUrl: 'https://www.instagram.com/herbakinglab/',
+        url: 'https://seattlebageloasis.com/index.html',
+        image: '/bagel-oasis.PNG',
+    },
+    {
+        name: 'Blazing Bagels - U-District',
+        photoCreditName: '@herbakinglab',
+        photoCreditUrl: 'https://www.instagram.com/herbakinglab/',
+        url: 'https://www.blazingbagels.com/',
+        image: '/blazing-bagels.PNG',
+    },
+    {
+        name: 'Dingfelders Delicatessen - Capitol Hill',
+        url: 'http://dingfelders.com/',
+        image: '/dingfelders.PNG',
+    },
+    {
+        name: 'Grateful Bread - N. U-District',
+        photoCreditName: '@herbakinglab',
+        photoCreditUrl: 'https://www.instagram.com/herbakinglab/',
+        url: 'https://gratefulbreadbaking.com/seattle/',
+        image: '/grateful-bread.PNG',
+    },
+    {
+        name: 'Little Lago - North Capitol Hill',
+        photoCreditName: '@herbakinglab',
+        photoCreditUrl: 'https://www.instagram.com/herbakinglab/',
+        url: 'http://littlelago.com/',
+        image: '/little-lago.PNG',
+    },
+    {
+        name: 'Macrina Bakery - Multiple Locations (bagels from Belltown)',
+        url: 'https://www.macrinabakery.com/',
+        image: '/macrina.PNG',
+    },
+    {
+        name: 'Porkchop & Co. - Ballard',
+        url: 'https://www.eatatporkchop.com/',
+        image: '/porkchopandco.PNG',
+    },
+    {
+        name: 'Rubinstein Bagels - Downtown',
+        url: 'https://rubinsteinbagels.com/',
+        image: '/rubinstein.PNG',
+    },
+    {
+        name: 'Schmaltzy’s Delicatessen - Ballard',
+        url: 'https://schmaltzysdeli.com/',
+        image: '/schmaltzys.PNG',
+    },
+    {
+        name: 'Seattle Bagel Bakery - Downtown',
+        photoCreditName: '@herbakinglab',
+        photoCreditUrl: 'https://www.instagram.com/herbakinglab/',
+        url: 'https://seattlebagel.com/',
+        image: '/seattle-bagel-bakery.PNG',
+    },
+    {
+        name: 'Shawn’s Cafe & Bakery',
+        image: '/shawns.PNG',
+    },
+    {
+        name: 'Westman’s Bagel & Coffee - Capitol Hill',
+        photoCreditName: '@herbakinglab',
+        photoCreditUrl: 'https://www.instagram.com/herbakinglab/',
+        url: 'http://www.westmansbagel.com/',
+        image: '/westmans.PNG',
+    },
+    {
+        name: 'Zylberschtein’s - Northgate',
+        url: 'https://www.zylberschtein.com/',
+        image: '/zylberschteins.PNG',
+    },
+];
 
 export default function Bakeries() {
     return (
@@ -15,8 +83,17 @@ export default function Bakeries() {
             {bakeries.map((bakery, i) => (
                 <div className="bakery" key={`bakery-${i}`}>
                     <img src={bakery.image} />
-                    <p className="name">{bakery.name}</p>
-                    <p className="description">{bakery.description}</p>
+                    {bakery.photoCreditName && (
+                        <span className="photo-credit">
+                            Photo:{' '}
+                            <a href={bakery.photoCreditUrl} target="_blank">
+                                {bakery.photoCreditName}
+                            </a>
+                        </span>
+                    )}
+                    <a href={bakery.url || '#'} target="_blank">
+                        <span className="name">{bakery.name}</span>
+                    </a>
                 </div>
             ))}
 
@@ -31,17 +108,25 @@ export default function Bakeries() {
                 }
 
                 .bakery .name {
-                    font-size: 120%;
+                    font-size: 110%;
                     margin: 1rem 0;
                 }
 
-                .bakery .description {
-                    margin: 0;
+                .bakery .photo-credit {
+                    font-size: 70%;
+                    display: block;
+                }
+                .bakery a {
+                    color: darkslateblue;
+                    text-decoration: none;
+                }
+                .bakery a:hover {
+                    text-decoration: underline;
                 }
 
                 .bakery img {
                     width: 450px;
-                    max-width: 100%;
+                    height: 450px;
                     border-radius: 4px;
                 }
 
