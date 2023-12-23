7472db8dcc18a3fbfb43b6baec525d0bfb0374de
Dylan Bussone <dylanbussone@gmail.com>
2023-12-23 15:04:40 -0800

    Use tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,18 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+// import Image from "next/image";
 
 export default function () {
   return (
-    <main className={styles.main}>
-      <h1>Bagel Quest</h1>
-      <h4>Coming soon</h4>
-      {/* <div className={styles.center}>
-        <Image
-          className={styles.logo}
+    <main className="flex flex-col items-center justify-between p-24">
+      <h1 className="mb-24 text-6xl font-semibold">Bagel Quest</h1>
+      <h4 className="text-xl font-semibold">Coming soon</h4>
+      {/* <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
           alt="Next.js Logo"
           width={180}
           height={37}
           priority
-        />
-      </div> */}
+        /> */}
     </main>
   );
 }
