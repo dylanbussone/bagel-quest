935aa738511b331d9f0327de003e1bb7c51e095a
Dylan Bussone <dylanbussone@gmail.com>
2023-12-23 18:05:47 -0800

    spinny bagel

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,13 @@ import Image from "next/image";
 export default function () {
   return (
     <div className="flex flex-col items-center justify-between">
-      <Image src="/bagel-icon-dark.svg" width={200} height={200} alt="bagel" />
+      <Image
+        src="/bagel-icon-dark.svg"
+        width={200}
+        height={200}
+        alt="bagel"
+        className="animate-spin-slow"
+      />
       <h1 className="text-5xl my-10 font-semibold">Bagel Quest</h1>
       <p>coming soon...</p>
     </div>
