53d0639de96c42e663d6963218187189961bd6e4
Dylan Bussone <dylanbussone@gmail.com>
2023-12-24 23:30:03 -0800

    header style

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -8,23 +8,32 @@ export default async function Header() {
   const session = await getServerSession(authOptions);
   const user = session?.user;
 
+  const headerLinkStyle =
+    "opacity-80 hover:opacity-100 transition duration-150 ease-in-out flex items-center";
+
   return (
     <header className="h-24 px-4 border-b bg-gray-800 text-white">
       <div className="max-w-screen-lg mx-auto h-full flex items-center justify-between">
-        <div className="flex gap-6 items-center">
-          <Link href="/" className="hover:underline">
+        <div className="flex gap-8 items-center">
+          <Link href="/" className={headerLinkStyle}>
             <Image
               src="/bagel-icon-light.svg"
               width={50}
               height={50}
               alt="bagel"
-              className="opacity-90 hover:opacity-100 transition duration-150 ease-in-out"
+              className="mr-2"
             />
+            <span>Bagel Quest</span>
           </Link>
           {user && (
-            <Link href="/order" className="hover:underline">
-              Order
-            </Link>
+            <>
+              <Link href="/order" className={headerLinkStyle}>
+                Order
+              </Link>
+              <Link href="/vote" className={headerLinkStyle}>
+                Vote
+              </Link>
+            </>
           )}
         </div>
         {user ? (
@@ -36,7 +45,7 @@ export default async function Header() {
                     src={user.image}
                     width={50}
                     height={50}
-                    className="rounded-full opacity-90 hover:opacity-100 transition duration-150 ease-in-out"
+                    className="rounded-full opacity-80 hover:opacity-100 transition duration-150 ease-in-out"
                     alt={`profile photo of ${user.name}`}
                     priority
                   />
