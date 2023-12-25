61010c5ac9fc3ca42762fc14a1f15b8870df04d6
Dylan Bussone <dylanbussone@gmail.com>
2023-12-24 23:30:14 -0800

    add vote page placeholder

---
+++ app/vote/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Vote() {
+  return (
+    <div className="flex justify-center items-center flex-col mt-8 sm:mt-20">
+      <h1 className="text-3xl mb-12 font-bold">Cast your votes</h1>
+      <p>Coming soon</p>
+    </div>
+  );
+}
