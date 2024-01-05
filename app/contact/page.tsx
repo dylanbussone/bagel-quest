9debf90355f96f8c4b3de8d1182938a4f0fd9745
Dylan Bussone <dylanbussone@gmail.com>
2024-01-05 15:49:15 -0800

    mailto

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,14 +1,19 @@
 export default function Contact() {
   return (
     <div className="flex justify-center items-center flex-col mt-8 sm:mt-20">
-      <h1 className="text-3xl mb-12 font-bold">Contact us</h1>
+      <h1 className="text-4xl sm:text-6xl py-4 font-bold text-transparent bg-clip-text bg-gradient-to-r from-amber-800 to-amber-600">
+        hey
+      </h1>
 
       <p className="text-center">
         Did we manage to get the attention of the press? Or do you just have a
         question?
         <br />
+        Feel free to reach out!
+        <br />
+        <br />
         <a href="mailto:srleviton@gmail.com" className="text-blue-800">
-          Feel free to reach out!
+          srleviton@gmail.com
         </a>
       </p>
     </div>
