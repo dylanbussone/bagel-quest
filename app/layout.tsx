c4f0febaff2a02443e5f8a4881391e84aba5807f
Dylan Bussone <dylanbussone@gmail.com>
2024-01-14 15:35:43 -0800

    Add metadataBase to metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://www.bagel-quest.com"),
   title: "Bagel Quest",
   description:
     "Bagel Quest is a bagel competition based in Seattle, Washington.",
